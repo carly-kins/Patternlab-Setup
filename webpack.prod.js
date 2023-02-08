5c2a2a2091e1a1a9e9da868cc8ce79957ef9e9f6
Carly Wooten <104537084+carly-kins@users.noreply.github.com>
2023-02-08 17:10:43 -0600

    update prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require("webpack-merge");
 const path = require("path");
 const common = require("./webpack.common.js");
 //const RemovePlugin = require('remove-files-webpack-plugin'); //https://www.npmjs.com/package/remove-files-webpack-plugin
-//const PattenlabWebpackPlugin = require("./.ideas/patternlab-webpack-plugin"); // -- Investigate PL plugin
 //TODO:
 // -- Investigate Dev vs Prod Config Files //https://webpack.js.org/guides/production/. minimize images on production build https://github.com/gruntjs/grunt-contrib-imagemin
 
@@ -10,7 +9,6 @@ module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
   //plugins: [
-    //new PattenlabWebpackPlugin ({command: "build",}),
     //new RemovePlugin({
     //before: {
     // parameters for "before normal compilation" stage.
